203418ea38f0b5e709795700554d60100e217581
GerardoGR <gerardo_1322134023@uptecamac.edu.mx>
2023-10-08 23:17:03 -0600

    feat: include api routes

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
+
+Route::get('/data', 'App\Http\Controllers\apiController@getData');
+Route::get('/discos', 'App\Http\Controllers\apiController@index'); //muestra todos los registros
+Route::post('/discos', 'App\Http\Controllers\apiController@store'); // crea un registro
+Route::put('/discos/{id}', 'App\Http\Controllers\apiController@update'); // actualiza un registro
+Route::delete('/discos/{id}', 'App\Http\Controllers\apiController@destroy'); //elimina un registro
